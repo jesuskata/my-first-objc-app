dd7d91464f2758b612922446ddd60bd1b10f4853
jesuskata <rzarate.alex@gmail.com>
2018-06-19 22:41:45 -0500

    This is my first commit for the test app in iOS Objective-C

---
+++ my-first-app/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  my-first-app
+//
+//  Created by Jesús Alejandro Romero Zárate on 19/06/18.
+//  Copyright © 2018 Jesús Alejandro Romero Zárate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ my-first-app/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  my-first-app
+//
+//  Created by Jesús Alejandro Romero Zárate on 19/06/18.
+//  Copyright © 2018 Jesús Alejandro Romero Zárate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
